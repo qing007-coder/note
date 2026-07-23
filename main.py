58550fd9eca4210f202175d7f7d89d679c85a6f0
xubo <1457800511@qq.com>
2026-07-24 00:20:29 +0800

    feat: 添加pytorch模块

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from numpy_.test import numpy_test
 from opencv.test import test_opencv
 from open3d_.test import test_open3d
+from pytorch.test import test_pytorch
 
 if __name__ == '__main__':
     # numpy_test()
     # test_opencv()
-    test_open3d()
+    # test_open3d()
+    test_pytorch()
